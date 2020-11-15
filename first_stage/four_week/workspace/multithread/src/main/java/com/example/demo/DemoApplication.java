b86e903f6000b12039307e635dd404e87317c833
apathyyi <shenyi_97@163.com>
2020-11-15 19:51:30 +0800

    第四周学习笔记和练习题

---
+++ first_stage/four_week/workspace/multithread/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,145 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        Thread1 mTh1=new Thread1("A");
+        Thread1 mTh2=new Thread1("B");
+        mTh1.start();
+        mTh2.start();
+
+        new Thread(new Thread2("C")).start();
+        new Thread(new Thread2("D")).start();
+
+
+        System.out.println(Thread.currentThread().getName()+"主线程运行开始!");
+        Thread1 mTh11=new Thread1("A");
+        Thread1 mTh22=new Thread1("B");
+        mTh11.start();
+        mTh22.start();
+        System.out.println(Thread.currentThread().getName()+ "主线程运行结束!");
+
+        Object a = new Object();
+        Object b = new Object();
+        Object c = new Object();
+        MyThreadPrinter2 pa = new MyThreadPrinter2("A", c, a);
+        MyThreadPrinter2 pb = new MyThreadPrinter2("B", a, b);
+        MyThreadPrinter2 pc = new MyThreadPrinter2("C", b, c);
+
+
+        new Thread(pa).start();
+        try {
+            Thread.sleep(100);  //确保按顺序A、B、C执行
+            new Thread(pb).start();
+            Thread.sleep(100);
+            new Thread(pc).start();
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+}
+class Thread1 extends Thread{
+    private String name;
+    public Thread1(String name) {
+        this.name=name;
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(name + "运行  :  " + i);
+            try {
+                sleep((int) Math.random() * 10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
+class Thread2 implements Runnable{
+    private String name;
+
+    public Thread2(String name) {
+        this.name=name;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(name + "运行  :  " + i);
+            try {
+                Thread.sleep((int) Math.random() * 10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
+
+class Thread3 extends Thread{
+    private String name;
+    public Thread3(String name) {
+        super(name);
+        this.name=name;
+    }
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " 线程运行开始!");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("子线程"+name + "运行 : " + i);
+            try {
+                sleep((int) Math.random() * 10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + " 线程运行结束!");
+    }
+}
+
+/**
+ * wait
+ */
+ class MyThreadPrinter2 implements Runnable {
+
+    private String name;
+    private Object prev;
+    private Object self;
+
+    public MyThreadPrinter2(String name, Object prev, Object self) {
+        this.name = name;
+        this.prev = prev;
+        this.self = self;
+    }
+
+    @Override
+    public void run() {
+        int count = 10;
+        while (count > 0) {
+            synchronized (prev) {
+                synchronized (self) {
+                    System.out.print(name);
+                    count--;
+
+                    self.notify();
+                }
+                try {
+                    prev.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+    }
